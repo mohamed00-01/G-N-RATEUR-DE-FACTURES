c996c67772eb58605ba7ab5667a2f78bf4693dda
mohamed <hammadachemohamed8@gmail.com>
2026-02-24 06:08:16 +0100

    changement

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# G-N-RATEUR-DE-FACTURES
+# GENERATEUR-DE-FACTURES
 Développement d’une application web permettant de créer et télécharger des factures au format PDF.
--- filejs.js
+++ filejs.js
@@ -1,7 +1,7 @@
-let products = []; // Tableau pour stocker les produits ajoutés
+let products = []; 
 let totalHT = 0;
 
-// Génération numéro facture
+
 document.getElementById("invoiceNumber").innerText =
     Math.floor(Math.random() * 100000);
 
@@ -33,15 +33,14 @@ function addProduct() {
 function renderTable() {
 
     const tbody = document.querySelector("#productTable tbody");
-    tbody.innerHTML = ""; //On vide le tableau. sinon, à chaque ajout de produit,
-                         //les anciennes lignes resteraient affichées en double.
+    tbody.innerHTML = ""; 
     totalHT = 0; 
 
 
 
-    products.forEach((item, index) => { //On parcourt le tableau products.
-        totalHT += item.total;    //recalcule le total complet apres avoir initialiser le 
-        // tebleau product a zero .   //index , position utile pour supprimer // item : produit actuel
+    products.forEach((item, index) => { 
+        totalHT += item.total;   
+        
 
         const row = ` 
         <tr>
@@ -51,9 +50,9 @@ function renderTable() {
             <td>${item.total.toFixed(2)}</td>
             <td><button onclick="deleteProduct(${index})">X</button></td>
         </tr>
-        `; //Ici on crée une ligne HTML dynamiquement.
+        `;
 
-        tbody.innerHTML += row; //On ajoute la ligne au tableau.
+        tbody.innerHTML += row; 
     });
 }
 
@@ -65,39 +64,36 @@ function deleteProduct(index) {
     saveToLocalStorage();
 }
 
-function updateTotals() {   //espace de calcul 
+function updateTotals() {  
     const rate = document.getElementById("tvaRate").value / 100;
     const tva = totalHT * rate;
     const totalTTC = totalHT + tva;
 
-    document.getElementById("totalHT").innerText = totalHT.toFixed(2); //<-- modifie les valeur
+    document.getElementById("totalHT").innerText = totalHT.toFixed(2); 
     document.getElementById("tva").innerText = tva.toFixed(2);
     document.getElementById("totalTTC").innerText = totalTTC.toFixed(2);
 }
 
 
-function saveToLocalStorage() { //C’est un espace de stockage intégré au navigateur.
+function saveToLocalStorage() { 
     localStorage.setItem("products", JSON.stringify(products));
-    //mon navigateur a un espace de stockage local appelé localStorage,
-    //  qui permet de stocker des données sous forme de paires clé - valeur.
+    
 
-}  //JSON.stringify() permet de convertir le tableau products en une chaîne de caractères JSON,
-//  qui peut être stockée dans le localStorage.
-//en gros mon tableau product est converti en une chaîne de caractères .
+}  
+
+
 
 function loadFromLocalStorage() {
-    const data = localStorage.getItem("products"); // je récupères la valeur stockée sous 
-    // la clé "products".
-    //Si rien n’a été sauvegardé, data sera null.
-    if (data) {      //Tu vérifies que des données existent
-        products = JSON.parse(data);   // reconvertis la chaîne JSON en vrai tableau JavaScript.
-         // apre ca product devient un tableau exploitable .
-
-         //Mise à jour de l’interface .
+    const data = localStorage.getItem("products");
+    
+    
+    if (data) {     
+        products = JSON.parse(data);  
+         
         renderTable();   
         updateTotals();
     }
-} //Sinon, si tu actualises la page → tout disparaît .
+} 
 
 loadFromLocalStorage();
 
